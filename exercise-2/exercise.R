41a694abdd428e0afcac6a5afa679a593d853082
Shelley Tsui <tsuishelley@gmail.com>
2017-01-10 17:42:55 -0800

    Completed exercise 2 for module 5

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,28 +1,29 @@
 # Exercise-2: more practice with basic syntax
 
 # Create a variable `food` that stores your favorite kind of food
-
+food <- "pizza"
 
 # Create a variable 'restaurant' that stores your favorite place to eat
-
+restaurant <- "Chipotle"
 
 # Create a variable `friends` equal to the number of friends you would like to eat with
-
+friends <- 1.0
 
 # Create a variable `meal.price`, which is how expensive you think one meal at the restaurant will be
-
+meal.price <- 7.0
 
 # Create a variable `total.cost` that has the total cost of your bill
-
+total.cost <- 14.0
 
 # Create a variable 'total.cost.tip' to be the total cost including a 15% tip
-
+total.cost.tip <- 16.1
 
 # Create a variable 'price.limit' set to your spending budget
-
+price.limit <- 20.0
 
 # Create a boolean variable `too.expensive`, set to TRUE if the cost with the tip is greater than the price limit
-
+too.expensive <- total.cost.tip > price.limit
 
 # Create a variable `max.friends`, which is the maximum number of friends you can invite that is in range
 # of your price limit
+max.friends <- 1.0
